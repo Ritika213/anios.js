f15f266d0779b28ea8eb096f76212cea2d416e44
Ritika213 <ritikakgupta6@gmail.com>
2023-11-05 17:34:09 +0530

    new comment

--- soya.js
+++ soya.js
@@ -123,8 +123,7 @@ function errorHandling() {
 
 // CANCEL TOKEN
 function cancelToken() {
-  axios
-  .get('http://jsonplaceholder.typicode.com/todos'),{
+  axio  .get('http://jsonplaceholder.typicode.com/todos'),{
   cancelToken : SourceBuffer.token
   .then(res=> showOutput(res))
   .catch(thrown=> {
@@ -143,40 +142,7 @@ function cancelToken() {
 // AXIOS INSTANCES
 
 // Show output in browser
-function showOutput(res) {
-  document.getElementById('res').innerHTML = `
-  <div class="card card-body mb-4">
-    <h5>Status: ${res.status}</h5>
-  </div>
 
-  <div class="card mt-3">
-    <div class="card-header">
-      Headers
-    </div>
-    <div class="card-body">
-      <pre>${JSON.stringify(res.headers, null, 2)}</pre>
-    </div>
-  </div>
-
-  <div class="card mt-3">
-    <div class="card-header">
-      Data
-    </div>
-    <div class="card-body">
-      <pre>${JSON.stringify(res.data, null, 2)}</pre>
-    </div>
-  </div>
-
-  <div class="card mt-3">
-    <div class="card-header">
-      Config
-    </div>
-    <div class="card-body">
-      <pre>${JSON.stringify(res.config, null, 2)}</pre>
-    </div>
-  </div>
-`;
-}
 
 // Event listeners
 document.getElementById('get').addEventListener('click', getTodos);
